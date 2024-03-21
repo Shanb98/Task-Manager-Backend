38a9cab5d9b3827314a4f141efa4041fc1328809
Shan Basnayake <shanbasnayake@hotmail.com>
2024-03-21 11:30:19 +0530

    adding jwt validator

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,7 +20,6 @@ const createuser = asyncHandler(async (req, res) => {
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    console.log("Hashed password:", hashedPassword);
 
     // Create a new user
     const user = await User.create({
@@ -68,6 +67,61 @@ const createuser = asyncHandler(async (req, res) => {
   } catch (error) {
     console.error('Error in registerAdmin:', error);
     res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+  try {
+    //fetching the email and password from the body
+    const { username, password } = req.body;
+    if (!username || !password) {
+      res.status(400);
+      throw new Error("All fields are mandatory!");
+    }
+
+    //taking the email from the req body and finding it from the db
+    const user = await User.findOne({ username });
+    console.log(user);
+
+    
+    // Check if the user exists
+    if (!user) {
+      res.status(404);
+      throw new Error("User not found");
+    }
+
+    //comparing the entered password with hashed password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (isPasswordValid) {
+      const accessToken = jwt.sign(
+        {
+          //payload
+          user: {
+            firstname: user.firstname,
+            lastname: user.lastname,
+            username: user.username,
+            role: user.role,
+            email: user.email,
+            agentNo:user.agentNo,
+            id: user._id,
+            registerId : user.registerId,
+          },
+        },
+        //access token secret
+        process.env.ACCESS_TOKEN_SECRET,
+        //expiration date
+        { expiresIn: "15m" }
+      );
+      console.log(process.env.ACCESS_TOKEN_SECRET);
+      res.status(200).json({ accessToken });
+    } else {
+      res.status(401);
+      throw new Error("Invalid password");
+    }
+  } catch (error) {
+    // Handle any errors that occur
+    console.error(error);
+    res.status(500).json({ message: "Invalid Login Details. Please try again!" });
   }
 });
 
---
+++ middleware/validateTokenHandler.js
@@ -0,0 +1,33 @@
+const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+
+const validateToken = asyncHandler(async (req, res, next) => {
+  let token;
+  let authHeader = req.headers.Authorization || req.headers.authorization;
+  if(authHeader && authHeader.startsWith("Bearer")){
+    //splitting the token in to two parts with the space and taking the second part which includes the token
+    token = authHeader.split(" ")[1];
+    
+    //verifying the token
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+            res.status(401);
+            throw new Error("User is not autherized");
+        }
+
+        //if the user is autherized
+        //take the info of the user and storing it in req.user
+        req.user = decoded.user;
+        console.log(req.user);
+        //append in the request body
+        next();
+    });
+
+    if (!token) {
+        res.status(401);
+        throw new Error("User is not authorized or token is missing");
+      }   
+}
+});
+
+module.exports = validateToken;
