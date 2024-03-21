f3cfc234d4469c6661b45837807a912eaee141de
Shan Basnayake <shanbasnayake@hotmail.com>
2024-03-21 11:59:30 +0530

    config cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectiondb = require("./config/dbConnection");
+const cors = require("cors");
 
 dotenv.config();
 connectiondb();
@@ -9,6 +10,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173" }));
+app.options("*", cors());
+
 app.use("/api/users", require("./routes/userRoutes"));
 
 app.listen(port, () => {
